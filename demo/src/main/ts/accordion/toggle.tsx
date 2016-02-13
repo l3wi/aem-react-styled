a98c5c0148bd86e2f509c73e7f15ae58dc5f5ce2
stefan meyer <stefan.meyer@sinnerschrader.com>
2016-02-13 10:06:03 +0100

    migrated accordion

--- demo/src/main/ts/accordion/toggle.tsx
+++ demo/src/main/ts/accordion/toggle.tsx
@@ -5,33 +5,30 @@ import CqUtils from "aem-react-js/CqUtils";
 import { ResourceInclude }  from "aem-react-js/include";
 
 
-interface ToggleProps extends Aem.AemProps {
-    resourceType: string;
-    label: string;
-    node: string;
+interface ToggleProps extends Aem.ResourceProps<any> {
     active: boolean;
-    path: string;
     key: string;
     groupId: string;
     onChange(): void;
+    cqHidden?: boolean;
 }
 
-export class Toggle extends Aem.AemComponent<ToggleProps, any> {
+export class Toggle extends Aem.ResourceComponent<any, ToggleProps, any> {
 
 
-    public render(): React.ReactElement<any> {
+    public renderBody(): React.ReactElement<any> {
         let onChange = function (): void {
             this.props.onChange();
         }.bind(this);
 
-        let label: string = this.props.label || "Set a Label";
+        let label: string = this.getResource().label || "Set a Label";
 
         if (this.isWcmEditable()) {
-            this.setAllEditableVisible(this.props.path, this.props.active && !this.props.cqHidden);
-            CqUtils.setVisible(this.props.path + "/togglepar/*", this.props.active && !this.props.cqHidden, true);
+            this.setAllEditableVisible(this.getPath(), this.props.active && !this.props.cqHidden);
+            CqUtils.setVisible(this.getPath() + "/togglepar/*", this.props.active && !this.props.cqHidden, true);
         }
 
-        let resourceType: string = this.props.resourceType;
+        let resourceType: string = this.getResourceType();
 
         // TODO CqEdit should be executed on client side only. also element should be set to outer div
 
@@ -45,15 +42,13 @@ export class Toggle extends Aem.AemComponent<ToggleProps, any> {
 
         return (
             <div className="toggle">
-                <EditDialog  path={this.props.path}
-                                 resourceType={resourceType}></EditDialog>
-                <input ref="toggleRadio" type="radio" className="toggle-input-state" checked={this.props.active} id={this.props.path}
+                <input ref="toggleRadio" type="radio" className="toggle-input-state" checked={this.props.active} id={this.getPath()}
                        name={this.props.groupId} onChange={onChange}/>
                 <label className="toggle-input-toggle toggle-input-js-toggle"
-                       htmlFor={this.props.path}>{label}
+                       htmlFor={this.getPath()}>{label}
                 </label>
                 <div className="toggle-input-content">
-                    <ResourceInclude  path={this.props.path + "/togglepar"}
+                    <ResourceInclude  path={this.getPath() + "/togglepar"}
                                      resourceType="/libs/foundation/components/parsys"></ResourceInclude>
                 </div>
             </div>
