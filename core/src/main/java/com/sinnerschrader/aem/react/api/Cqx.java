d546e8414efbd4ef577d88af8d76574df4d16cde
stefan meyer <stefan.meyer@sinnerschrader.com>
2016-10-28 10:03:08 +0200

    pass resource path of current resource to Java Api

--- core/src/main/java/com/sinnerschrader/aem/react/api/Cqx.java
+++ core/src/main/java/com/sinnerschrader/aem/react/api/Cqx.java
@@ -9,57 +9,57 @@ package com.sinnerschrader.aem.react.api;
  */
 public class Cqx {
 
-  private Sling sling;
-  private OsgiServiceFinder finder;
-  private ModelFactory modelFactory;
+	private Sling sling;
+	private OsgiServiceFinder finder;
+	private ModelFactory modelFactory;
 
-  public Cqx(Sling sling, OsgiServiceFinder finder, ModelFactory modelFactory) {
-    super();
-    this.sling = sling;
-    this.finder = finder;
-    this.modelFactory = modelFactory;
-  }
+	public Cqx(Sling sling, OsgiServiceFinder finder, ModelFactory modelFactory) {
+		super();
+		this.sling = sling;
+		this.finder = finder;
+		this.modelFactory = modelFactory;
+	}
 
-  /**
-   * get an osgi service
-   *
-   * @param name
-   *          fully qualified class name
-   * @return
-   */
-  public JsProxy getOsgiService(String name) {
-    return finder.get(name);
-  }
+	/**
+	 * get an osgi service
+	 *
+	 * @param name
+	 *            fully qualified class name
+	 * @return
+	 */
+	public JsProxy getOsgiService(String name) {
+		return finder.get(name);
+	}
 
-  /**
-   * get a request sling model
-   *
-   * @param name
-   *          fully qualified class name
-   * @return
-   */
-  public JsProxy getRequestModel(String name) {
-    return modelFactory.createRequestModel(name);
-  }
+	/**
+	 * get a request sling model
+	 *
+	 * @param name
+	 *            fully qualified class name
+	 * @return
+	 */
+	public JsProxy getRequestModel(String path, String name) {
+		return modelFactory.createRequestModel(path, name);
+	}
 
-  /**
-   * get a resource sling model
-   *
-   * @param name
-   *          fully qualified class name
-   * @return
-   */
-  public JsProxy getResourceModel(String name) {
-    return modelFactory.createResourceModel(name);
-  }
+	/**
+	 * get a resource sling model
+	 *
+	 * @param name
+	 *            fully qualified class name
+	 * @return
+	 */
+	public JsProxy getResourceModel(String path, String name) {
+		return modelFactory.createResourceModel(path, name);
+	}
 
-  /**
-   * get access to resource via the sling objects
-   * 
-   * @return
-   */
-  public Sling getSling() {
-    return sling;
-  }
+	/**
+	 * get access to resource via the sling objects
+	 * 
+	 * @return
+	 */
+	public Sling getSling() {
+		return sling;
+	}
 
 }
--- core/src/main/java/com/sinnerschrader/aem/react/api/ModelFactory.java
+++ core/src/main/java/com/sinnerschrader/aem/react/api/ModelFactory.java
@@ -2,6 +2,8 @@ package com.sinnerschrader.aem.react.api;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.adapter.Adaptable;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.wrappers.SlingHttpServletRequestWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,55 +16,63 @@ import org.slf4j.LoggerFactory;
  */
 public class ModelFactory {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(ModelFactory.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(ModelFactory.class);
 
-  private ClassLoader classLoader;
+	private ClassLoader classLoader;
 
-  public ModelFactory(ClassLoader classLoader, SlingHttpServletRequest request) {
-    super();
-    this.classLoader = classLoader;
-    this.request = request;
-  }
+	public ModelFactory(ClassLoader classLoader, SlingHttpServletRequest request) {
+		super();
+		this.classLoader = classLoader;
+		this.request = request;
+	}
 
-  private SlingHttpServletRequest request;
+	private SlingHttpServletRequest request;
 
-  /**
-   * adapts the current request to the given class
-   *
-   * @param className
-   *          fully qualified class name
-   * @return
-   */
-  public JsProxy createRequestModel(String className) {
-    return createModel(className, request);
-  }
+	/**
+	 * adapts the current request to the given class
+	 *
+	 * @param className
+	 *            fully qualified class name
+	 * @return
+	 */
+	public JsProxy createRequestModel(String path, String className) {
+		return createModel(className, new SlingHttpServletRequestWrapper(request) {
 
-  /**
-   * adapts the current resource to the given class
-   *
-   * @param className
-   *          fully qualified class name
-   * @return
-   */
-  public JsProxy createResourceModel(String className) {
-    return createModel(className, request.getResource());
-  }
+			@Override
+			public Resource getResource() {
+				return request.getResourceResolver().getResource(path);
+			}
 
-  private JsProxy createModel(String className, Adaptable adapatable) {
+		});
+	}
 
-    Class<?> clazz;
-    try {
-      clazz = classLoader.loadClass(className);
-    } catch (ClassNotFoundException e) {
-      LOGGER.error("could not find model class " + className);
-      return null;
-    }
-    Object object = adapatable.adaptTo(clazz);
+	/**
+	 * adapts the current resource to the given class
+	 *
+	 * @param className
+	 *            fully qualified class name
+	 * @return
+	 */
+	public JsProxy createResourceModel(String path, String className) {
 
-    if (object == null) {
-      return null;
-    }
-    return new JsProxy(object, clazz);
-  }
+		return createModel(className, request.getResourceResolver().getResource(path));
+	}
+
+	private JsProxy createModel(String className, Adaptable adapatable) {
+
+		Class<?> clazz;
+		try {
+			clazz = classLoader.loadClass(className);
+		} catch (ClassNotFoundException e) {
+			LOGGER.error("could not find model class " + className);
+			return null;
+		}
+		Object object = adapatable.adaptTo(clazz);
+
+		if (object == null) {
+			return null;
+		}
+		return new JsProxy(object, clazz);
+	}
 
 }
